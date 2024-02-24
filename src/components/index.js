000a9bcde4be9fb3cf6cba0f1aedf2598ed245a1
12Uttam <12uttamdarekar@gmail.com>
2024-02-24 20:26:21 +0530

    First commit

---
+++ src/components/index.js
@@ -0,0 +1,32 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import Input from "./Input";
+import Button from "./Button";
+import Select from "./Select";
+import RTE from "./RTE";
+import SignUp from "./SignUp";
+import Login from "./Login";
+import PostForm from "./post-form/PostForm";
+import AuthLayout from "./AuthLayout";
+
+import PostCard from "./PostCard";
+
+export {
+  Header,
+  Footer,
+  Container,
+  Logo,
+  LogoutBtn,
+  Input,
+  Button,
+  Select,
+  RTE,
+  SignUp,
+  Login,
+  PostForm,
+  PostCard,
+  AuthLayout,
+};
